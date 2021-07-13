8c794531f23f9971b321963cf0f967ced5ab48e5
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-07-12 21:40:51 -0300

    Criando as animaçoes de entrada e alguns hovers.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+const quitPopup = document.getElementById('closelogo')
+const background = document.getElementById('logo-background')
+quitPopup.addEventListener('click',function(){
+const container = document.getElementById('content')
+const body = document.getElementsByTagName('body')
+const backgroundAnimation = document.querySelector('.logo-image')
+body[0].classList.add('body-newcolor')
+container.classList.add('content--visible')
+backgroundAnimation.classList.add("logo--hidden")
+
+quitPopup.style.display ='none'
+setTimeout(function() {
+    background.style.display = 'none'
+    body[0].style.background = 'rgb(2, 54, 24)'
+    container.style.opacity = '100%'
+
+},2000)
+})
